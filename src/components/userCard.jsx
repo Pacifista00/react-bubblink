bae40cb73939d798666e360a54ed14ab71f0dc36
Pacifista00 <adamsamudera99@gmail.com>
2024-02-16 17:59:56 +0700

    designing user card

--- src/components/userCard.jsx
+++ src/components/userCard.jsx
@@ -1,7 +1,24 @@
+import Logo from "../assets/logo/logo.png";
+
 function userCard() {
   return (
     <aside className="user-card sticky-top p-4">
-      <div className="bg-dark text-light">hlo</div>
+      <div className="card-sidebar card sticky-top text-light bg-dark rounded-0">
+        <div className="card-body mx-auto">
+          <img
+            src={Logo}
+            className="profile-picture-aside rounded-circle m-3"
+            alt="profile"
+          />
+          <h3 className="text-center py-0 my-0">Pacifista</h3>
+          <p className="text-center text-secondary">Admin</p>
+        </div>
+        <form>
+          <button type="submit" className="text-light my-btn py-2 fs-5">
+            Logout
+          </button>
+        </form>
+      </div>
     </aside>
   );
 }
